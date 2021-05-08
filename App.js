206e217ea73d494d1374db3c568262fd244714e7
Isaiah Obi <zedlinkus@gmail.com>
2021-05-08 01:04:50 +0100

    Layout Positioning

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.search}>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight,
+      },
+      ios: {
+        marginTop: 0,
+      },
+      default: {
+        marginTop: 20,
+        marginBottom: 20,
+      },
+    }),
+  },
+  search: {
+    flex: 1,
+    backgroundColor: "green",
+    padding: 20,
+  },
+  list: {
+    flex: 8,
+    backgroundColor: "blue",
+    padding: 20,
   },
 });
